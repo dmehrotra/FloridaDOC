a1f3a551431de8134305c26e61f65431d7779599
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2018-09-19 06:33:58 -0400

    comments etc

--- parse.py
+++ parse.py
@@ -1,42 +1,88 @@
 import os
 from bs4 import BeautifulSoup
 from row import Row
+import csv
 
 folder = "data"
+csv_rows=[]
 
-def ninety_six(filename):
-  soup = BeautifulSoup(open(os.path.join(folder, filename)), "html.parser")
+def ninety_six(f):
+  """
+    Straight forward parsing of 1995-1996.html file
+  """
+  soup = BeautifulSoup(open(os.path.join(folder, f)), "html.parser")
   table = soup.findAll("table")[1]
   rows = table.findChildren(['tr'])
+  
+  for data in rows:
+    # skip table headers
+    if len(data.findChildren(["th"])) < 1:
+      csv_rows.append(Row("1996", data, 0, 7))
+
+def twenty_six(f):
+  """
+  The file I scraped for 2005-2006 data categorizes correctional facilities into four categorizes
+  with four corresponding tables. This loops through the four tables, retrieves the data, and tallys
+  the incarcerated population of a given county 
+  """
+  soup = BeautifulSoup(open(os.path.join(folder, f)), "html.parser")
+  
+  for t in [8,12,14,15]:
+    table = soup.findAll("table")[t]
+    rows = table.findChildren(['tr'])
+    handle_irregular(rows)
+
+def twenty_fifteen(f):
+  soup = BeautifulSoup(open(os.path.join(folder, f)), "html.parser")
+  table = soup.findAll("table")[1]
+  rows = table.findChildren(['tr'])
+  
+  for data in rows:
+    # skip table headers
+    if len(data.findChildren(["th"])) < 1:
+      csv_rows.append(Row("2015", data, 0, 1))
+
+def handle_irregular(rows):
+  """
+    Some ugly logic to handle 2005-2006 data.
+  """
+  
   for data in rows:
     if len(data.findChildren(["th"])) < 1:
-      print(Row("1996",data).county)
+      row = Row("2006", data, 6, 7)
+      if any(r.year == "2006" and r.county == row.county for r in csv_rows) and row.county is not None:
+        matching_row = next((match for match in csv_rows if match.year == "2006" and match.county == row.county ), None)
+        matching_row.population = matching_row.population + row.population
+      else:
+        csv_rows.append(row)
 
-def twenty_six(filename):
-    return "February"
-
-def twenty_fifteen(filename):
-    return "March"
-
-def parse(argument):
-    file_map = {
-        "1995-1996.html": ninety_six(filename),
-        "2005-2006.html": twenty_six(filename),
-        "2014-2015.html": twenty_fifteen(filename)
-    }
-    # Get the function from switcher dictionary
-    func = file_map.get(argument, lambda: "Invalid File")
-    # Execute the function
-    print (func())
+def parse(filename):
+  """
+  Map the filename to a function and call the function. 
+    1. The idea here is that if we want to add more counties later we can do so here
+    2. also the assumption here is any scraped html can't be parsed the same way. 
+  """
+  file_map = {
+      '1995-1996.html': ninety_six,
+      '2005-2006.html': twenty_six,
+      '2014-2015.html': twenty_fifteen
+  }
+  func = file_map.get(filename, lambda: "Invalid File")
+  func(filename)
 
 for filename in os.listdir(folder):
+  """
+  Loop through files in the data directory and parse them. 
+  """
   parse(filename)
 
+with open("prisonpop-by-county.csv", 'w') as csv_file:
+    """ 
+    classic csv writer
+    """
+    wr = csv.writer(csv_file, delimiter=',')
+    wr.writerow(["Year", "County", "Population"])
+    for cdr in csv_rows:
+      if cdr.county is not None:
+        wr.writerow([cdr.year,cdr.county,cdr.population])
 
-
-
-
-
-
-
-# import code; code.interact(local=dict(globals(), **locals()))
--- row.py
+++ row.py
@@ -2,19 +2,34 @@ from bs4 import BeautifulSoup
 
 class Row(object):
   """
+  This is a class to build each row of data.  
+    1. The idea behind the class is that its an easy way to extend 
+       the row if we want more data in the future
   """
 
-  def __init__(self, year, row):
+  def __init__(self, year, row, county, pop):
     self.year = year
-    self.county = self.get_county(row)
+    self.data = row
+    self.county = self.get_county(county)
+    self.population = self.get_population(pop)
 
-
-  def get_county(self, row):
-    if self.year != '2006':
-      return row.findAll(["td"])[0]
-    
-    
-
-
-
+  def get_county(self, county):
+    """
+      retrieves the county from the table row.  
+        1. It includes an exception for 2006, whose table differs from other years.
+    """
+    if self.year == '2006':
+      if len(self.data.findAll(["td"])) == 8:
+        return self.data.findAll(["td"])[county].text.lstrip()
+    else:
+      return self.data.findAll(["td"])[county].text.lstrip()
+  
+  def get_population(self, pop):
+    if self.year == '2006':
+      if len(self.data.findAll(["td"])) == 8:
+        clean = self.data.findAll(["td"])[pop].text.lstrip().replace(',', '')
+        return int(clean)
+    else:
+      clean = self.data.findAll(["td"])[pop].text.lstrip().replace(',', '')
+      return int(clean)
 
